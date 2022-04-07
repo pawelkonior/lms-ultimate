d98110388664f7a60b7e300ec9f8a76865e42883
Paweł Konior <pawel.konior@gmail.com>
2022-04-07 13:04:14 +0200

    add pytest configuration and base tests with db fixure, custom user fixture as generator

---
+++ users/tests/test_models.py
@@ -0,0 +1,8 @@
+def test_create_user(user, django_user_model):
+    users = django_user_model.objects.all()
+    assert len(users) == 1
+
+
+def test_change_password(user):
+    user.set_password('secret')
+    assert user.check_password('secret') is True
