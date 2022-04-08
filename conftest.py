c6a6b89242b040b12af8a8db4ddc4b3c293dcfef
Paweł Konior <pawel.konior@gmail.com>
2022-04-08 11:35:12 +0200

    add custom migrations for groups and super user from envs

--- conftest.py
+++ conftest.py
@@ -1,9 +1,7 @@
 import pytest
-from django.contrib.auth.models import Group
 
 
 @pytest.fixture(scope='function')
 def user(db, django_user_model):
     """User instance from default django user model"""
-    Group.objects.create(name='students')
     yield django_user_model.objects.create_user(email='a@a.pl', fullname='Ala', password='testPass123')
--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -1,6 +1,6 @@
 def test_create_user(user, django_user_model):
     users = django_user_model.objects.all()
-    assert len(users) == 1
+    assert len(users) == 2
 
 
 def test_change_password(user):
